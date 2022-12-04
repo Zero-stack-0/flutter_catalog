3e6967b11f1cf96527ba0477899a9422e655a557
Shubham Jangid <shubhamjangid@192.168.1.3>
2022-12-04 14:10:04 +0530

    Form validator

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_demo/utils/routes.dart';
 
 class Homepage extends StatelessWidget {
@@ -7,13 +8,63 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool isButtonClicked = false;
     return Scaffold(
       appBar: AppBar(
           title: Text(
         "Catalog App",
         style: TextStyle(fontWeight: FontWeight.bold),
       )),
-      body: Center(),
+      body: Padding(
+        padding: const EdgeInsets.all(80),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Center(
+              // ignore: prefer_const_literals_to_create_immutables
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    InkWell(
+                        onTap: () {
+                          isButtonClicked = true;
+                        },
+                        child: AnimatedContainer(
+                            duration: Duration(seconds: 1),
+                            alignment: Alignment.center,
+                            height: 100,
+                            width: 150,
+                            decoration: BoxDecoration(
+                                // ignore: prefer_const_literals_to_create_immutables
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: Colors.grey,
+                                      blurRadius: 10.0,
+                                      offset: Offset.zero)
+                                ],
+                                color: isButtonClicked
+                                    ? Colors.green
+                                    : Colors.blueAccent,
+                                borderRadius: BorderRadius.circular(25)),
+                            child: Image.asset(
+                              "assets/images/about_us.png",
+                              width: 200,
+                              fit: BoxFit.cover,
+                            ))),
+                    SizedBox(height: 30),
+                    Container(
+                      child: Text(
+                        "This is about us information about our company",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    )
+                  ]),
+            ),
+          ],
+        ),
+      ),
       drawer: Drawer(
         child: ListView(padding: EdgeInsets.zero, children: [
           DrawerHeader(
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -13,94 +13,114 @@ class Loginpage extends StatefulWidget {
 }
 
 class _LoginpageState extends State<Loginpage> {
-  String Name = "";
+  final _formKey = GlobalKey<FormState>();
 
+  String Name = "";
   bool isButtonClicked = false;
   @override
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
         child: SingleChildScrollView(
-            child: Column(
-          children: [
-            Image.asset(
-              "assets/images/login_image.png",
-              fit: BoxFit.cover,
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            Text(
-              "Welcome $Name",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 26.0, horizontal: 28.0),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                        hintText: "Enter username", labelText: "Usename"),
-                    onChanged: (value) {
-                      Name = value;
-                      setState(() {});
-                    },
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: "Enter your password",
-                      labelText: "Password",
+            child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    Image.asset(
+                      "assets/images/login_image.png",
+                      fit: BoxFit.cover,
                     ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-
-                  InkWell(
-                    splashColor: Color.fromARGB(255, 177, 138, 183),
-                    onTap: () async {
-                      setState(() {
-                        isButtonClicked = true;
-                      });
-                      await Future.delayed(Duration(seconds: 1));
-                      Navigator.pushNamed(context, MyRoutes.HomeRoute);
-                    },
-                    child: AnimatedContainer(
-                      duration: Duration(seconds: 1),
-                      decoration: BoxDecoration(
-                          color: Colors.purple,
-                          borderRadius: BorderRadius.circular(20)),
-                      height: 40,
-                      width: isButtonClicked ? 100 : 150,
-                      alignment: Alignment.center,
-                      child: isButtonClicked
-                          ? Icon(
-                              Icons.done,
-                              color: Colors.white,
-                            )
-                          : Text("Login",
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 16)),
+                    SizedBox(
+                      height: 50,
                     ),
-                  )
+                    Text(
+                      "Welcome $Name",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 26.0, horizontal: 28.0),
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            decoration: InputDecoration(
+                                hintText: "Enter username",
+                                labelText: "Usename"),
+                            onChanged: (value) {
+                              Name = value;
+                              setState(() {});
+                            },
+                            validator: ((value) {
+                              if (value!.isEmpty) {
+                                return "Username should not be empty";
+                              }
+                            }),
+                          ),
+                          TextFormField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              hintText: "Enter your password",
+                              labelText: "Password",
+                            ),
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return "password cannot be empty";
+                              }
 
-                  // ElevatedButton(
-                  //     onPressed: () {
-                  //
-                  //     },
-                  //     child: Text("Login"),
-                  //     style: TextButton.styleFrom(minimumSize: Size(100, 40)))
-                ],
-              ),
-            )
-          ],
-        )));
+                              if (value.length < 6) {
+                                return "password should be greater than 6";
+                              }
+                            },
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          InkWell(
+                            splashColor: Color.fromARGB(255, 177, 138, 183),
+                            onTap: () async {
+                              setState(() {
+                                isButtonClicked = true;
+                              });
+
+                              if (_formKey.currentState!.validate()) {
+                                await Future.delayed(Duration(seconds: 1));
+                                // ignore: use_build_context_synchronously
+                                Navigator.pushNamed(
+                                    context, MyRoutes.HomeRoute);
+                                setState(() {
+                                  isButtonClicked = false;
+                                });
+                              }
+                              return null;
+                            },
+                            child: AnimatedContainer(
+                              duration: Duration(seconds: 1),
+                              decoration: BoxDecoration(
+                                  color: Colors.purple,
+                                  borderRadius: BorderRadius.circular(20)),
+                              height: 40,
+                              width: isButtonClicked ? 100 : 150,
+                              alignment: Alignment.center,
+                              child: isButtonClicked
+                                  ? Icon(
+                                      Icons.done,
+                                      color: Colors.white,
+                                    )
+                                  : Text("Login",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16)),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ))));
   }
 }
